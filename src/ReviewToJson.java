4823cad778b6a15bb1066eee9921ab539c6f129b
Leonard Hövelmann <laeoiv@gmail.com>
2016-05-14 10:21:02 +0200

    initial commit

---
+++ src/ReviewToJson.java
@@ -0,0 +1,88 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ReviewToJson {
+
+	public static void main(String args[]) {
+
+		class Item {
+
+			public Item() {
+				this.word_count = new HashMap<>();
+			}
+
+			public String sentiment;
+			public Map<String, Long> word_count;
+
+		}
+
+		if (args.length != 1) {
+			System.out
+					.println("You have to provide the path to the .review file. An example call is java -jar reviewtojson.jar \"positive.review\"");
+			return;
+		}
+
+		String filename = args[0].split("\\.")[0];
+
+		BufferedReader br = null;
+
+		// for each array: index at 0 : the word, index at 1 : the count, index
+		// at 2 : the class
+		List<Item> resultList = new LinkedList<Item>();
+
+		try {
+
+			String currentLine;
+
+			br = new BufferedReader(new FileReader(args[0]));
+			System.out.println("File loaded: " + args[0]);
+			System.out.println("Extracting information");
+			while ((currentLine = br.readLine()) != null) {
+				String sentiment = currentLine.substring(
+						currentLine.length() - 8, currentLine.length()).equals(
+						"positive") ? "1" : "0";
+				currentLine = currentLine.substring(0,
+						currentLine.length() - 17);
+				String[] substr = currentLine.split("\\s+");
+				Item item = new Item();
+				item.sentiment = sentiment;
+				for (String s : substr) {
+					String[] ss = s.split(":");
+					if (item.word_count.containsKey(ss[0]))
+						item.word_count.put(ss[0], item.word_count.get(ss[0])
+								+ Integer.valueOf(ss[1]));
+					else
+						item.word_count.put(ss[0], Long.valueOf(ss[1]));
+				}
+				resultList.add(item);
+			}
+
+			System.out.println("Extraction done");
+			System.out.println("Saving in file " + args[0] + ".json");
+
+			ObjectMapper mapper = new ObjectMapper();
+
+			mapper.writeValue(new File(filename + ".json"), resultList);
+
+			System.out.println("Saved " + resultList.size() + " sentiments.");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)
+					br.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+}
